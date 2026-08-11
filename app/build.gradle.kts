8f6264d7ea1e7e9474d69a808d508e666714f256
XingOfficial <xing20260731@yeah.net>
2026-08-11 21:59:43 +0800

    Remove old signing config, use GitHub Actions signing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,12 +22,6 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
-            signingConfig = signingConfigs.create("release") {
-                storeFile = file(System.getenv("KEYSTORE_PATH") ?: "debug.keystore")
-                storePassword = System.getenv("KEYSTORE_PASSWORD") ?: "android"
-                keyAlias = System.getenv("KEY_ALIAS") ?: "androiddebugkey"
-                keyPassword = System.getenv("KEY_PASSWORD") ?: "android"
-            }
         }
     }
 
